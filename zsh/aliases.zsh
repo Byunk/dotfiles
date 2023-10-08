c181e528fcf98eb242eddeae0d25d3edca635266
Byunk <kyungho.byoun@gmail.com>
2023-10-08 20:24:26 +0900

    fix nvm wrong path error

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,8 +8,8 @@ alias g++='g++ -std=c++17'
 alias t='todo.sh'
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # shell
 bindkey "^[^[[D" forward-word
