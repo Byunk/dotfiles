4953eee35eb83557484b4716dc15d10e3ca849e8
Byunk <kyungho.byoun@gmail.com>
2024-01-22 23:23:49 +0900

    update fzf

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,6 +7,9 @@ alias k='kubectl'
 alias g++='g++ -std=c++17'
 alias t='todo.sh'
 
+# fzf
+export FZF_DEFAULT_OPTS="--height 40%"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
