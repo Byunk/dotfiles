83e927606f466c3aa1eee97c6d0659b579dd2fb9
Byunk <kyungho.byoun@gmail.com>
2024-05-27 14:29:09 +0900

    fix fzf installation script

--- fzf/install.sh
+++ fzf/install.sh
@@ -7,10 +7,12 @@ DOTFILES_ROOT=$(pwd -P)
 source "${DOTFILES_ROOT}/scripts/common.sh"
 
 PACKAGE="fzf"
+VERSION="0.52.1"
 
 if is.linux && is.amd; then
-	uri="https://github.com/junegunn/fzf/releases/download/0.52.1/fzf-0.52.1-linux_amd64.tar.gz"
-	curl -LO $uri $PACKAGE
-	tar -xzvf $PACKAGE
-	mv $PACKAGE /bin/$PACKAGE
+	OS="linux_amd64"
+	RELEASE="fzf-$VERSION-$OS.tar.gz"
+	URI="https://github.com/junegunn/fzf/releases/download/$VERSION/$RELEASE"
+	curl -LO $URI
+	mkdir -p "$DOTFILES_ROOT/bin" && tar -xzvf "$RELEASE" -C "$DOTFILES_ROOT/bin"
 fi
