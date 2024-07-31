4e6651c2d59d494ea106ac91dac116be54f76f6e
Byunk <kyungho.byoun@gmail.com>
2024-07-31 11:45:04 +0900

    chore: change bash to zsh in gvm installation

--- gvm/install.sh
+++ gvm/install.sh
@@ -1,1 +1,2 @@
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+
