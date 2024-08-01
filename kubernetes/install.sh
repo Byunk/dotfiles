337cc555ccbf400b04a6aab35dc5466ec01bd170
Byunk <kyungho.byoun@gmail.com>
2024-08-01 14:00:26 +0900

    fix installation script for kubectl

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -10,6 +10,7 @@ ARCH=$(getArch)
 RELEASE="https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/$OS/$ARCH/kubectl"
 
 curl -sSL -o /tmp/kubectl "$RELEASE"
+chmod u+x /tmp/kubectl
 mv /tmp/kubectl "$(dirname "$(dirname "$0")")/bin/kubectl"
 
 logInfo "kubectl installed successfully"
