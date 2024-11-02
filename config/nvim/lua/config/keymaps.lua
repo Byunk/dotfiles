7296638b6e191f563c2f2fb6dd6b1367b941c5c4
Byunk <kyungho.byoun@gmail.com>
2024-11-02 22:50:12 +0900

    Update nvim config

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Meta + left/right to move by word
+map("n", "<Esc>b", "b")
+map("n", "<Esc>f", "w")
