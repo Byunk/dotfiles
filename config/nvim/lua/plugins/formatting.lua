1a6736cee028086b147cca6741cce8a9012803ec
Byunk <kyungho.byoun@gmail.com>
2024-11-05 01:24:57 +0900

    Refactor nvim-cmp config and add formatters for lua, fish, and sh in Neovim setup

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -4,6 +4,9 @@ return {
 		opts = function()
 			local opts = {
 				formatters_by_ft = {
+					lua = { "stylua" },
+					fish = { "fish_indent" },
+					sh = { "shfmt" },
 					go = { "goimports", "gofmt" },
 					python = { "isort", "black" },
 				},
