bb129f7f87eb6df3f23f4ddf2b27093946399d96
Byunk <kyungho.byoun@gmail.com>
2024-11-06 00:37:42 +0900

    Fix error handling in get_latest_release function and improve directory removal safety in install script.

--- installers/common.sh
+++ installers/common.sh
@@ -37,9 +37,14 @@ getos() {
 # get the latest release tag from github
 # https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'
+  local version
+  version=$(curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                                      # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/')
+  if [[ -z "$version" ]]; then
+    error "Failed to get latest release version"
+  fi
+  echo "$version"
 }
 
 # compare two versions
